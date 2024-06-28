57abb4fb9f08db198d78519f400a8605d0c27627
narakushi <taina.santana1305@gmail.com>
2024-06-27 23:38:52 -0300

    consertando bug de verificação com if els

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,41 @@
-const timer = document.querySelector('.timer');
-let relogio;
-let sec = 0;
+function relogio() {
+    const timer = document.querySelector('.timer');
+    let relogio;
+    let sec = 0;
 
-const getTimeBySeconds = (second) => {
-    const data = new Date(second * 1000);
-    return data.toLocaleTimeString('pt-BR', { hour12: false, timeZone: 'GMT' })
+    const getTimeBySeconds = (second) => {
+        const data = new Date(second * 1000);
+        return data.toLocaleTimeString('pt-BR', { hour12: false, timeZone: 'GMT' })
+    }
+
+    const iniciaRelogio = () => {
+        relogio = setInterval(() => {
+            sec++;
+            timer.innerHTML = getTimeBySeconds(sec);
+        }, 1000);
+    }
+
+    document.addEventListener("click", (e) => {
+        const el = e.target;
+
+        if (el.classList.contains('iniciar')) {
+            timer.classList.remove('pause');
+            clearInterval(relogio);
+            iniciaRelogio();
+        }
+
+        else if (el.classList.contains('pausar')) {
+            clearInterval(relogio);
+            timer.classList.add('pause');
+        }
+
+        else if(el.classList.contains('zerar')){
+            timer.classList.remove('pause');
+            clearInterval(relogio);
+            sec = 0;
+            timer.innerHTML = '00:00:00';
+        }
+    })
 }
 
-const iniciaRelogio = () => {
-    relogio = setInterval(() => {
-        sec++;
-        timer.innerHTML = getTimeBySeconds(sec);
-    }, 1000);
-}
-
-document.addEventListener("click", (e) => {
-    const el = e.target;
-
-    if (el.classList.contains('iniciar')) {
-        clearInterval(relogio);
-        iniciaRelogio();
-    }
-
-    else if(el.classList.contains('pausar')) {
-        clearInterval(relogio);
-    }
-
-    else {
-        clearInterval(relogio);
-        sec = 0;
-        timer.innerHTML = '00:00:00';
-    }
-})
+relogio();
